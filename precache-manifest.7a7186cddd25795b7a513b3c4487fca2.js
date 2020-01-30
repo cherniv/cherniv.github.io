e8ae840706e036fc80929248b2c19b296a378939
Ivan <cherniv@gmail.com>
2020-01-30 10:57:13 +0200

    Hello Bestrx

---
+++ precache-manifest.7a7186cddd25795b7a513b3c4487fca2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dde2cbbb5ba84332996ae549f02f7f0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05008759b1cdd1b77068",
+    "url": "/static/css/main.1fd50bdc.chunk.css"
+  },
+  {
+    "revision": "bbc34661e017ecf6b6da",
+    "url": "/static/js/2.6f52008a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.6f52008a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "05008759b1cdd1b77068",
+    "url": "/static/js/main.fd7d475c.chunk.js"
+  },
+  {
+    "revision": "f96371d7213df3e7904f",
+    "url": "/static/js/runtime-main.4fe7a48c.js"
+  }
+]);
